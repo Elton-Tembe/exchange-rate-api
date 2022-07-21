4a73d194015362688765e12ee73e45711972acae
Elton-Tembe <eltontembe10@gmail.com>
2022-07-21 16:14:57 +0300

    Adding datasource initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,26 @@
+INSERT INTO rate (id, from_currency, to_currency, exchange_rate) VALUES 
+(1, 'EUR', 'GBP', 0.851),
+(2, 'EUR', 'MZN', 65.32),
+(3, 'EUR', 'USD', 1.02),
+(4, 'EUR', 'ZAR', 17.45),
+
+(5, 'GBP', 'EUR', 1.17),
+(6, 'GBP', 'MZN', 76.6),
+(7, 'GBP', 'USD', 1.2),
+(8, 'GBP', 'ZAR', 20.5),
+
+(9, 'MZN', 'EUR', 0.0153),
+(10, 'MZN', 'GBP', 0.0131),
+(11, 'MZN', 'USD', 0.0156),
+(12, 'MZN', 'ZAR', 0.267),
+
+(13, 'USD', 'EUR', 0.98),
+(14, 'USD', 'GBP', 0.835),
+(15, 'USD', 'MZN', 64.11),
+(16, 'USD', 'ZAR', 17.11),
+
+(17, 'ZAR', 'EUR', 0.0573),
+(18, 'ZAR', 'GBP', 0.0488),
+(19, 'ZAR', 'MZN', 3.74),
+(20, 'ZAR', 'USD', 0.0584);
+;
